22c51d7f7b3150d4550b9c90f3f7f4a4e71dea64
Asanga Chan <cspl.itm@googlemail.com>
2017-11-29 01:27:25 +0530

    changed 2 files

--- Lession2/src/app/app.component.ts
+++ Lession2/src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>I am {{name}}</h1>
+  <p>i am in {{age1}} now but doing like {{age2}} </p>
+  <p>{{greetingText}}</p>
+  `,
 })
 export class AppComponent {
   title = 'app';
+  name1:string = "Asanga";
+  age1:string = "30's";
+  age2:string = "20's";
+  greetingText:string = "hehehe just kidding";
+
+  clickSomeButton(){console.log("Welcome to Lesson 2");}
 }
